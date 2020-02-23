64da52f9eb290c9bbd95676eb58230fe36b7a4a1
EricLuo <lykismekk@outlook.com>
2020-02-23 15:45:35 +0800

    Initial commit

---
+++ src/Ex3_LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters_Slow.java
@@ -0,0 +1,26 @@
+package Ex3_LongestSubstringWithoutRepeatingCharacters;
+
+public class LongestSubstringWithoutRepeatingCharacters_Slow {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        String tempStr = s.substring(0, 1);
+        int maxLen = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (tempStr.contains(s.substring(i, i + 1))) {
+                int index = tempStr.indexOf(s.substring(i, i + 1));
+                tempStr = tempStr.substring(index + 1);
+            }
+            tempStr += "" + s.substring(i, i + 1);
+            if (tempStr.length() > maxLen) {
+                maxLen = tempStr.length();
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        LongestSubstringWithoutRepeatingCharacters_Slow use = new LongestSubstringWithoutRepeatingCharacters_Slow();
+        System.out.println(use.lengthOfLongestSubstring("aab"));
+    }
+}
