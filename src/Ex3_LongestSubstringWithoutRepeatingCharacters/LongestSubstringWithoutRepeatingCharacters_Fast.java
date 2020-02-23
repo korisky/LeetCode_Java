8c432cfe7ff41e434ea4138e756fd3e2324e9e00
EricLuo <lykismekk@outlook.com>
2020-02-23 16:58:49 +0800

    Ex3 Fast implementation

--- src/Ex3_LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters_Fast.java
+++ src/Ex3_LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters_Fast.java
@@ -1,26 +1,29 @@
 package Ex3_LongestSubstringWithoutRepeatingCharacters;
 
-public class LongestSubstringWithoutRepeatingCharacters_Slow {
+public class LongestSubstringWithoutRepeatingCharacters_Fast {
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0)
             return 0;
-        String tempStr = s.substring(0, 1);
-        int maxLen = 1;
-        for (int i = 1; i < s.length(); i++) {
-            if (tempStr.contains(s.substring(i, i + 1))) {
-                int index = tempStr.indexOf(s.substring(i, i + 1));
-                tempStr = tempStr.substring(index + 1);
-            }
-            tempStr += "" + s.substring(i, i + 1);
-            if (tempStr.length() > maxLen) {
-                maxLen = tempStr.length();
+
+        char[] charStr = s.toCharArray();
+        int maxLen = 0;
+        int left = 0;
+        int checker = 0;
+
+        for (int j = 0; j < charStr.length; j++){
+            for (checker = left; checker < j; checker++){
+                if (charStr[checker] == charStr[j]){
+                    maxLen = Math.max(maxLen, j - left);
+                    left = checker + 1;
+                }
             }
         }
-        return maxLen;
+
+        return Math.max(maxLen, charStr.length - left);
     }
 
     public static void main(String[] args) {
-        LongestSubstringWithoutRepeatingCharacters_Slow use = new LongestSubstringWithoutRepeatingCharacters_Slow();
+        LongestSubstringWithoutRepeatingCharacters_Fast use = new LongestSubstringWithoutRepeatingCharacters_Fast();
         String test = "asdbasbuiabweibiuebfleuaksfkjsdbkcjbkjcbkbkzxckSBKDCBKsbduiqwsakdsakdhasjkdhkjasdhjkasdhjksadhjkadhjkasdjkhasjkdhasjkdhjkasdhasjkdhas";
 
         long startTime = System.currentTimeMillis();
