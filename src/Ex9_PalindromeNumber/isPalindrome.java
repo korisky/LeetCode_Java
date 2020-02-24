9cf0e2695408c02c95067277bbee132fa749f6d2
EricLuo <lykismekk@outlook.com>
2020-02-24 19:40:12 +0800

    Ex8, 9 Pass

---
+++ src/Ex9_PalindromeNumber/isPalindrome.java
@@ -0,0 +1,16 @@
+package Ex9_PalindromeNumber;
+
+public class isPalindrome {
+    public boolean isPalindrome(int x) {
+        StringBuffer sb = new StringBuffer(String.valueOf(x));
+        if (sb.reverse().toString().equals(String.valueOf(x)))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        isPalindrome use = new isPalindrome();
+        System.out.println(use.isPalindrome(121));
+    }
+}
---
+++ src/Ex9_PalindromeNumber/isPalindrome_Fast.java
@@ -0,0 +1,25 @@
+package Ex9_PalindromeNumber;
+
+/*
+remember: for INT number, the fastest way to reverse it, is to % 10 and * 10 stuff.
+Using this method can much more faster than convert it into String an reverse.
+ */
+
+public class isPalindrome_Fast {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int rev = 0;
+        int tempUsing = x;
+        while (tempUsing != 0){
+            rev = rev * 10 + tempUsing % 10;
+            tempUsing /= 10;
+        }
+        return rev == x;
+    }
+
+    public static void main(String[] args) {
+        isPalindrome_Fast use = new isPalindrome_Fast();
+        System.out.println(use.isPalindrome(-121));
+    }
+}
