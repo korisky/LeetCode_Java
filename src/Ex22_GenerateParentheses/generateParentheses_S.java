4ca2b0c7806f2e1675b4745b3931a3ea4603ec9a
EricLuo <lykismekk@outlook.com>
2020-02-28 12:27:34 +0800

    Ex22 Pass

---
+++ src/Ex22_GenerateParentheses/generateParentheses_S.java
@@ -0,0 +1,43 @@
+package Ex22_GenerateParentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+By using recursion to find out all posible combinations
+we got three situations :
+                1. ( and ) 's number all equal to n, we need to record it
+                2. ('s num equal to n, but we still get ), we need to parse all )
+        IMP!!!  3. If ( and ) < n, then we can traverse by: left delete then right then delete
+                    BUT, if current ( > ), which means: e.g. ()), ()()), we using ) more than (
+                    which is incorrect, thus, we need : ( & ) > n && ( < )
+ */
+
+public class generateParentheses_S {
+    private List<String> stores;
+    public List<String> generateParenthesis(int n) {
+        stores = new ArrayList<>();
+        if (n > 0)
+            pushBra(n, n, new StringBuffer());
+        return stores;
+    }
+    public void pushBra(int leftRemaining, int rightRemaining, StringBuffer sb) {
+        if (leftRemaining == 0 && rightRemaining == 0) {
+            stores.add(sb.toString());
+        } else if (leftRemaining == 0 && rightRemaining != 0) {
+            pushBra(leftRemaining, rightRemaining - 1, sb.append(")"));
+            sb.deleteCharAt(sb.length() - 1);
+        } else if (leftRemaining != 0 && rightRemaining != 0 && leftRemaining <= rightRemaining) {
+            pushBra(leftRemaining - 1, rightRemaining, sb.append("("));
+            sb.deleteCharAt(sb.length() - 1);
+            pushBra(leftRemaining, rightRemaining - 1, sb.append(")"));
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+    public static void main(String[] args) {
+        generateParentheses_S use = new generateParentheses_S();
+        List<String> results = use.generateParenthesis(1);
+        for (String s : results)
+            System.out.println(s);
+    }
+}
