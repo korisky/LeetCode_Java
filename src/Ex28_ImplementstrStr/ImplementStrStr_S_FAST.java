a42ca30ed74aae2aca338875035fce952fad228d
EricLuo <lykismekk@outlook.com>
2020-03-01 10:24:11 +0800

    Ex28 Pass

---
+++ src/Ex28_ImplementstrStr/ImplementStrStr_S_FAST.java
@@ -0,0 +1,15 @@
+package Ex28_ImplementstrStr;
+
+public class ImplementStrStr_S_FAST {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+        ImplementStrStr_S_FAST use = new ImplementStrStr_S_FAST();
+        String input = "hello";
+        String find = "ll";
+
+        System.out.println(use.strStr(input, find));
+    }
+}
