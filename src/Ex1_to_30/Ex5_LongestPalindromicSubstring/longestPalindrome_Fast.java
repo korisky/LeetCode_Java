ddd28b31f0f31a476828ab4426cf5e5821ca2052
EricLuo <lykismekk@outlook.com>
2020-05-22 18:20:18 +0800

    Finished Ex5 with new implementation (Much more faster)

--- src/Ex1_to_30/Ex5_LongestPalindromicSubstring/longestPalindrome_Fast.java
+++ src/Ex1_to_30/Ex5_LongestPalindromicSubstring/longestPalindrome_Fast.java
@@ -1,47 +1,61 @@
 package Ex1_to_30.Ex5_LongestPalindromicSubstring;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class longestPalindrome_Fast {
     public String longestPalindrome(String s) {
-        char[] sArr = s.toCharArray();
-        StringBuffer sb = new StringBuffer();
-        sb.append("#");
-        for (int i = 0; i < sArr.length; i++) {
-            sb.append(sArr[i]);
-            sb.append("#");
-        }
-        int[] lenArr = new int[sb.length()];
+
+        if (s == null || s.length() <= 1)
+            return s;
 
         // Manacher's Algorithm
-        int centre = 0;
-        int rightBound = 0;
-        int maxLength = 0;
-        int realCentre = 0;
-        for (int i = 0; i < sb.length() - 1; i++) { // sb[sb.length() - 1] is #, we do not need to check it
-            int mirror = 2 * centre - i;
-            // mirror position got at least it's mirror's length
-            if (i < rightBound)
-                lenArr[i] = Math.min(lenArr[mirror], rightBound - i);
-                // according to Manacher's Algorithm's Graph.....
-            while ( i - lenArr[i] - 1 >= 0 && i + lenArr[i] + 1 < sb.length()
-                    && sb.charAt(i + lenArr[i] + 1) == sb.charAt(i - lenArr[i] - 1))
-                lenArr[i]++; // keep expending the palindrome
-            if (i + lenArr[i] > rightBound){
-                centre = i; // re-direct the centre (cause it has longer palindrome)
-                rightBound = i + lenArr[i];
-                if (maxLength < lenArr[i]){
-                    maxLength = lenArr[i];
-                    realCentre = i;
-                }
+        char[] arr = s.toCharArray();
+        StringBuilder sb = new StringBuilder("#");
+        for (char c : arr) {
+            sb.append(c);
+            sb.append("#");
+        }
 
+        int centre = 0;
+        int right_boundary = 0;
+        int max_len = 0;
+        int max_centre = 0;
+        int[] palin_len = new int[sb.length()];
+
+        for (int i = 0; i < sb.length() - 1; i++) { // sb[sb.length() - 1] is #, we do not need to check it
+            int mirror = 2 * centre - i; // mirror position got at least it's mirror's length
+
+            if (i < right_boundary)
+                palin_len[i] = Math.min(right_boundary - i, palin_len[mirror]);
+                // according to Manacher's Algorithm's Graph.....
+
+            while (i - palin_len[i] - 1 >= 0 && i + palin_len[i] + 1 < sb.length() // Careful here! its < not <=
+                    && sb.charAt(i - palin_len[i] - 1) == sb.charAt(i + palin_len[i] + 1))
+                palin_len[i]++;
+                // keep expending the palindrome
+
+            if (i + palin_len[i] > right_boundary) {
+                // re-direct the centre (cause it has longer palindrome)
+                centre = i;
+                right_boundary = i + palin_len[i];
+                if (palin_len[i] > max_len) {
+                    max_centre = i;
+                    max_len = palin_len[i];
+                }
             }
         }
-        char[] ret = sb.subSequence(realCentre - maxLength, realCentre + maxLength).toString().toCharArray();
-        String result = "";
-        for (int i = 0; i < ret.length; i++){
-            if (ret[i] != '#')
-                result += ret[i];
+
+        // Here we use Pattern and Matcher to do character replacement in StringBuffer
+        // Which is much more faster then doing .replaceAll() with String.
+        Pattern p = Pattern.compile("#");
+        Matcher m = p.matcher(sb.substring(max_centre - max_len, max_centre + max_len));
+        StringBuffer  result = new StringBuffer ();
+        while (m.find()){
+            m.appendReplacement(result, "");
         }
-        return result;
+        m.appendTail(result);
+        return result.toString();
     }
 
     public static void main(String[] args) {
