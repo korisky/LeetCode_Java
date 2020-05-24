f64d9eff3764994df071af50959d5895359ea88e
EricLuo <lykismekk@outlook.com>
2020-05-24 16:41:44 +0800

    Ex19 Slightly improvement && much clear implementation

--- src/Ex1_to_30/Ex19_RemoveNthNodeFromEndOfList/removeNthFromEnd.java
+++ src/Ex1_to_30/Ex19_RemoveNthNodeFromEndOfList/removeNthFromEnd.java
@@ -19,44 +19,46 @@ public class removeNthFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if (n == 0 || head.next == null)
             return null;
-        int temp = n;
-        int count = 0;
-        ListNode slower = head;
-        ListNode faster = head;
-        while (temp-- + 1 != 0 && faster != null) {
-            faster = faster.next;
-            count++;
+
+        ListNode preHead = new ListNode(0);
+        preHead.next = head;
+
+        ListNode fast = preHead;
+        ListNode slow = preHead;
+        // slow pointer is used to point to the one before the
+        // one we want to delete
+
+        while (n > 0) {
+            if (fast.next == null)
+                return null; // the target we need to delete is greater of length of the linked list
+            fast = fast.next;
+            n--;
         }
-        while (faster != null) {
-            faster = faster.next;
-            slower = slower.next;
-            count++;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        if (count == n)
-            return head.next;
-        if (slower.next != null)
-            slower.next = slower.next.next;
-        return head;
+
+        slow.next = slow.next.next;
+
+        return preHead.next;
     }
 
 
     public static void main(String[] args) {
-//        ListNode t5 = new ListNode(5);
-//        ListNode t4 = new ListNode(4);
-//        t4.next = t5;
-//        ListNode t3 = new ListNode(3);
-//        t3.next = t4;
-//        ListNode t2 = new ListNode(2);
-//        t2.next = t3;
-//        ListNode t1 = new ListNode(1);
-//        t1.next = t2;
-
-//        ListNode t2 = new ListNode(2);
+        ListNode t5 = new ListNode(5);
+        ListNode t4 = new ListNode(4);
+        t4.next = t5;
+        ListNode t3 = new ListNode(3);
+        t3.next = t4;
+        ListNode t2 = new ListNode(2);
+        t2.next = t3;
         ListNode t1 = new ListNode(1);
-//        t1.next = t2;
+        t1.next = t2;
 
         removeNthFromEnd use = new removeNthFromEnd();
-        ListNode newHead = use.removeNthFromEnd(t1, 1);
+        ListNode newHead = use.removeNthFromEnd(t1, 6);
         while (newHead != null) {
             System.out.println(newHead.val);
             newHead = newHead.next;
