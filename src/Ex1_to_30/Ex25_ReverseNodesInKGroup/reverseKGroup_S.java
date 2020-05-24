9e4c320c00534bbafa404026da83326da2c7385f
EricLuo <lykismekk@outlook.com>
2020-05-24 21:41:23 +0800

    Ex25 special situation encounter

--- src/Ex1_to_30/Ex25_ReverseNodesInKGroup/reverseKGroup_S.java
+++ src/Ex1_to_30/Ex25_ReverseNodesInKGroup/reverseKGroup_S.java
@@ -21,28 +21,38 @@ public class reverseKGroup_S {
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head == null)
             return null;
-        ListNode countLen = head;
-        int len = 0;
-        while (countLen != null) {
-            len++;
-            countLen = countLen.next;
-        }
-        if (len < k)
-            return head;
-        // else, we need to reverse it
-        ListNode cur = head;
-        ListNode pre = null;
+
+        // do counting first
         ListNode temp = head;
-        int count = 0;
-        while (cur != null && count < k) {
-            count++;
-            ListNode nextNode = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = nextNode;
+        int listLength = 0;
+        boolean longEnough = false;
+        while (temp != null) {
+            listLength++;
+            if (listLength > k) { /////////// special situation!!!!!
+                                    //////// If we use listLength++ in if statement instead of current code
+                                    /////// we cannot get full reverse.
+                longEnough = true;
+                break;
+            }
+            temp = temp.next;
         }
-        head.next = reverseKGroup(cur, k);
-        return pre;
+
+        if (!longEnough)
+            return head;
+
+        // then we need to reverse it
+        // we need to do the reverse every k's part
+        ListNode curNode = head;
+        ListNode preNode = null;
+        int count = 0;
+        while (curNode != null && count++ < k) {
+            ListNode nextNode = curNode.next;
+            curNode.next = preNode;
+            preNode = curNode;
+            curNode = nextNode;
+        }
+        head.next = reverseKGroup(curNode, k);
+        return preNode;
     }
 
 
