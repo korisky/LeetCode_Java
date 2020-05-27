9ed2680ceece5d67b4338a0c481e55e53a426f0a
EricLuo <lykismekk@outlook.com>
2020-05-27 20:41:58 +0800

    Ex1306 finished with Own & Faster implementation with idea on LeetCode

---
+++ src/Ex1306_JumpGame3/FasterSolution.java
@@ -0,0 +1,13 @@
+package Ex1306_JumpGame3;
+
+public class FasterSolution {
+    public boolean canReach(int[] arr, int start) {
+        if (arr != null && start >= 0 && start < arr.length) {
+
+            int jump = arr[start]; // if jump == 0, means we can reach a 0
+            arr[start] = arr.length; // make this slot 'dead', no longer with any valid jump
+            return jump == 0 || canReach(arr, start - jump) || canReach(arr, start + jump);
+        }
+        return false;
+    }
+}
---
+++ src/Ex1306_JumpGame3/OwnSolution.java
@@ -0,0 +1,36 @@
+package Ex1306_JumpGame3;
+
+import java.util.HashSet;
+
+public class OwnSolution {
+    public boolean canReach(int[] arr, int start) {
+        if (start < 0 || start >= arr.length)
+            return false;
+        boolean[] met = new boolean[arr.length];
+        checking(arr, start, met);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0 && met[i])
+                return true;
+        }
+        return false;
+    }
+
+    public void checking(int[] arr, int curIndex, boolean[] met) {
+        // once we call this function, means we met this slot
+        met[curIndex] = true;
+        int left = curIndex - arr[curIndex];
+        int right = curIndex + arr[curIndex];
+
+        if (left >= 0 && !met[left])
+            checking(arr, left, met);
+        if (right < arr.length && !met[right])
+            checking(arr, right, met);
+    }
+
+    public static void main(String[] args) {
+        OwnSolution use = new OwnSolution();
+        int[] arr = new int[]{4, 2, 3, 0, 3, 1, 2};
+        int start = 5;
+        System.out.println(use.canReach(arr, start));
+    }
+}
