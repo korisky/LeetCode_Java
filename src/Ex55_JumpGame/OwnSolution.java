1f268118ecf668a9cc1f6e86b64570b81e2b4111
EricLuo <lykismekk@outlook.com>
2020-05-27 15:33:14 +0800

    Ex55 Finished + faster & fastest implementation with LeetCode's idea

---
+++ src/Ex55_JumpGame/OwnSolution.java
@@ -0,0 +1,38 @@
+package Ex55_JumpGame;
+
+/*
+    Too slow for execution
+ */
+
+public class OwnSolution {
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return false;
+        else
+            return doJumpping(nums, 0);
+    }
+
+    public boolean doJumpping(int[] nums, int curPos) {
+        if (curPos == nums.length - 1)
+            return true;
+        else if (curPos > nums.length - 1)
+            return false;
+
+        int maxJump = nums[curPos];
+        for (int jumpStep = 1; jumpStep <= maxJump; jumpStep++) {
+            if (doJumpping(nums, curPos + jumpStep))
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        OwnSolution use = new OwnSolution();
+
+        int[] test_can = new int[]{2,3,1,1,4};
+        System.out.println(use.canJump(test_can));
+
+        int[] test_cannot = new int[]{3,2,1,0,4};
+        System.out.println(use.canJump(test_cannot));
+    }
+}
