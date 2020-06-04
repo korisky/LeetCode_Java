761e1053ed1bd149cf05cf59f744b9e5889261a1
EricLuo <lykismekk@outlook.com>
2020-06-04 10:32:40 +0800

    Ex189 finished with idea from LeetCode

---
+++ src/Ex189_RotateArray/Solution.java
@@ -0,0 +1,30 @@
+package Ex189_RotateArray;
+
+/*
+    Idea form LeetCode:
+        we can use three reverse to get what we want
+ */
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution use = new Solution();
+        int[] test = new int[]{1, 2, 3, 4, 5, 6, 7};
+    }
+}
