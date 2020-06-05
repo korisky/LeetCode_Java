1c9fbc7240c90453feb9486f7dd6f6dd9c06681b
EricLuo <lykismekk@outlook.com>
2020-06-05 11:12:41 +0800

    Ex980 finished with DFS method

---
+++ src/Ex980_UniquePathsIII/Solution.java
@@ -0,0 +1,60 @@
+package Ex980_UniquePathsIII;
+
+/*
+    On a 2-dimensional grid, there are 4 types of squares:
+
+    1 represents the starting square.  There is exactly one starting square.
+    2 represents the ending square.  There is exactly one ending square.
+    0 represents empty squares we can walk over.
+    -1 represents obstacles that we cannot walk over.
+    Return the number of 4-directional walks from the starting square to the ending square,
+    that walk over "every non-obstacle" square exactly once.
+
+ */
+
+import java.util.HashSet;
+
+public class Solution {
+    private int res = 0;
+    private int empty = 1;
+    private int sx, sy, ex, ey;
+
+    public int uniquePathsIII(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0)
+                    empty++;
+                else if (grid[i][j] == 1) {
+                    sx = i;
+                    sy = j;
+                } else if (grid[i][j] == 2) {
+                    ex = i;
+                    ey = j;
+                }
+            }
+        }
+        dfs(grid, sx, sy);
+        return res;
+    }
+
+    public void dfs(int[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length
+                || y < 0 || y >= grid[0].length
+                || grid[x][y] < 0)
+            return;
+        if (x == ex && y == ey) {
+            if (empty == 0) res++;
+            return;
+        }
+        grid[x][y] = -2;
+        empty--;
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
+        grid[x][y] = 0;
+        empty++;
+    }
+}
