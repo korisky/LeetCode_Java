1232f7a97c6d5edf5104b5f69ce39f4f562e91e7
lykis <lykismekk@outlook.com>
2020-06-25 21:35:54 +0800

    Ex100 slightly change

--- src/Ex100_SameTree/Solution.java
+++ src/Ex100_SameTree/Solution.java
@@ -22,7 +22,7 @@ public class Solution {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null || q == null)
-            return p == null;
+            return p == q;
         if (p.val == q.val)
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         return false;
