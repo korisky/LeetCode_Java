53ee9075c34a6eea597e60df550c0c0d6293afd2
lykis <lykismekk@outlook.com>
2020-06-28 13:59:18 +0800

    Ex206 finished

---
+++ src/Ex206_ReverseLinkedList/Solution.java
@@ -0,0 +1,30 @@
+package Ex206_ReverseLinkedList;
+
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
