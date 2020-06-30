39bc52773f33186ba1a51c72f0b10283e0f6228a
lykis <lykismekk@outlook.com>
2020-06-30 12:05:27 +0800

    Ex144 finished

---
+++ src/Ex144_BinaryTreePreorderTraversal/Solution.java
@@ -0,0 +1,43 @@
+package Ex144_BinaryTreePreorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null)
+            stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            res.add(root.val);
+            if (root.right != null)
+                stack.push(root.right);
+            if (root.left != null)
+                stack.push(root.left);
+        }
+        return res;
+    }
+}
