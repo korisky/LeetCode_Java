52e4a02b6a35007f734655823d4a950653396c90
lykis <lykismekk@outlook.com>
2020-07-03 10:18:41 +0800

    Ex102 finished

---
+++ src/Ex102_BinaryTreeLevelOrderTraversal/Solution.java
@@ -0,0 +1,50 @@
+package Ex102_BinaryTreeLevelOrderTraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> theQueue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root != null)
+            theQueue.offer(root);
+
+        while (!theQueue.isEmpty()) {
+            int curLevelNodeNum = theQueue.size();
+            List<Integer> curLevelNodes = new ArrayList<>();
+            for (int i = 0; i < curLevelNodeNum; i++) {
+                TreeNode cur = theQueue.poll();
+                if (cur.left != null)
+                    theQueue.offer(cur.left);
+                if (cur.right != null)
+                    theQueue.offer(cur.right);
+                curLevelNodes.add(cur.val);
+            }
+            res.add(curLevelNodes);
+        }
+        return res;
+    }
+}
