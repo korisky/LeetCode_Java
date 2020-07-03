421d44b4aef4d8ed9275f0d3840fd43f3ad28c66
lykis <lykismekk@outlook.com>
2020-07-03 09:10:44 +0800

    Ex101 finished with idea from Leetcode

---
+++ src/Ex99_RecoverBinarySearchTree/Solution.java
@@ -0,0 +1,74 @@
+package Ex99_RecoverBinarySearchTree;
+
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public void recoverTree(TreeNode root) {
+        TreeNode pre = null;
+        TreeNode first = null, second = null;
+        // Morris Traversal
+        TreeNode temp;
+        while (root != null) {
+            if (root.left != null) {
+                // connect threading for root
+                temp = root.left;
+                while (temp.right != null && temp.right != root)
+                    temp = temp.right;
+                // the threading already exists
+                if (temp.right != null) {
+                    if (pre != null && pre.val > root.val) {
+                        if (first == null) {
+                            first = pre;
+                            second = root;
+                        } else {
+                            second = root;
+                        }
+                    }
+                    pre = root;
+
+                    temp.right = null;
+                    root = root.right;
+                } else {
+                    // construct the threading
+                    temp.right = root;
+                    root = root.left;
+                }
+            } else {
+                if (pre != null && pre.val > root.val) {
+                    if (first == null) {
+                        first = pre;
+                        second = root;
+                    } else {
+                        second = root;
+                    }
+                }
+                pre = root;
+                root = root.right;
+            }
+        }
+        // swap two node values;
+        if (first != null && second != null) {
+            int t = first.val;
+            first.val = second.val;
+            second.val = t;
+        }
+    }
+}
