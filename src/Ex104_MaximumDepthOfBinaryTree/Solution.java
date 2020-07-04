e224e45e1755218bd700e5b07bb480c65266495b
lykis <lykismekk@outlook.com>
2020-07-04 21:33:22 +0800

    Ex104 finished with really simple idea from Leetcode

---
+++ src/Ex104_MaximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,31 @@
+package Ex104_MaximumDepthOfBinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
--- src/Ex993_CousinsInBinaryTree/Solution.java
+++ src/Ex993_CousinsInBinaryTree/Solution.java
@@ -1,8 +1,6 @@
 package Ex993_CousinsInBinaryTree;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class Solution {
