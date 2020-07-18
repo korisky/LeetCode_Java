151dddd4accc35c85b0c1bb5a5d1199f90b38ec6
lykis <lykismekk@outlook.com>
2020-07-18 15:07:00 +0800

    Interface stuff testing

---
+++ src/Extras/InterfaceDemo.java
@@ -0,0 +1,13 @@
+package Extras;
+
+public interface InterfaceDemo {
+    public int val = 5;
+    static int valval = 20;
+
+    public void test();
+
+    public default void testing() {
+    }
+
+    ;
+}
