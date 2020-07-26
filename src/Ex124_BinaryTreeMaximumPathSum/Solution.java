9eeeab622ed81adcda2f6a6ac5aa57843f1e0004
lykis <lykismekk@outlook.com>
2020-07-26 12:06:01 +0800

    Ex129 finished + improvement from LeetCode + deeper understanding of Ex124

--- src/Ex124_BinaryTreeMaximumPathSum/Solution.java
+++ src/Ex124_BinaryTreeMaximumPathSum/Solution.java
@@ -1,7 +1,7 @@
 package Ex124_BinaryTreeMaximumPathSum;
 
 public class Solution {
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -21,7 +21,7 @@ public class Solution {
     }
 
 
-    int maxPath = Integer.MIN_VALUE;
+    public static int maxPath = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
         helper(root);
@@ -38,5 +38,22 @@ public class Solution {
         maxPath = Math.max(maxPath, root.val + leftMax + rightMax);
 
         return root.val + Math.max(leftMax, rightMax);
+        // another tricky part, if we still go up from this node (include the current parent node and its ancestors)
+        // we can only choose left or right path + current val, but not left + right
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(7);
+        TreeNode n2 = new TreeNode(15);
+        TreeNode n3 = new TreeNode(20);
+        n3.left = n1;
+        n3.right = n2;
+        TreeNode n4 = new TreeNode(9);
+        TreeNode root = new TreeNode(-10);
+        root.left = n4;
+        root.right = n3;
+
+        Solution use = new Solution();
+        System.out.println(use.maxPathSum(root));
     }
 }
