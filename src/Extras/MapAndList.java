eac9f87b851ddb03a4f8038927b0db99e5e4b995
lykis <lykismekk@outlook.com>
2020-07-29 12:40:02 +0800

    Add some collection stuff

---
+++ src/Extras/MapAndList.java
@@ -0,0 +1,18 @@
+package Extras;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapAndList {
+    public static void main(String[] args) {
+
+        // Map
+        Map<Integer, Integer> theHashMap = new HashMap<>();
+        Map<Integer, Integer> theHashTable = new Hashtable<>();
+        Map<Integer, Integer> theTreeMap = new TreeMap<>();
+
+
+    }
+}
