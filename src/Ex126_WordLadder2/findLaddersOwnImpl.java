aecda728e7525a2d6376a6b920848f1e723be138
lykismekk <lykismekk@outlook.com>
2021-06-12 18:12:25 +0800

    add stuff testing

---
+++ src/Ex126_WordLadder2/findLaddersOwnImpl.java
@@ -0,0 +1,12 @@
+package Ex126_WordLadder2;
+
+import java.util.List;
+
+public class findLaddersOwnImpl {
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+
+        return null;
+    }
+
+}
