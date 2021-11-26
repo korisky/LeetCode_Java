24f819e8470e9dd6dbdcb05e91d5a6cfd5c9b831
lykismekk <lykismekk@outlook.com>
2021-11-26 22:18:12 +0800

    Ex93 dfsSolution for finding all valid ip

--- src/Extras/FlexportPreparing/IpAddress.java
+++ src/Extras/FlexportPreparing/IpAddress.java
@@ -1,6 +1,6 @@
 package Extras.FlexportPreparing;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -69,31 +69,93 @@ public class IpAddress {
 
         IpAddress test = new IpAddress();
 
-        // for question 1
-        List<String> testList = new LinkedList<>();
-        // valid
-        testList.add("123.1.2.40");
-        testList.add("255.1.2.40");
-        testList.add("255.255.255.255");
-        // invalid
-        testList.add("123.256.2.40");
-        testList.add("023.35.001.40");
-        testList.add("123.1.240");
-        testList.add("0.0.0.0");
-        testList.add("1");
-
-        for (String s : testList) {
-//            System.out.println("Ip: " + s + ", is " + test.checkIpValidity(s));
-            System.out.println("Ip: " + s + ", is " + test.checkIpValidityByRegularExpression(s));
-        }
+//        // for question 1
+//        List<String> testList = new LinkedList<>();
+//        // valid
+//        testList.add("123.1.2.40");
+//        testList.add("255.1.2.40");
+//        testList.add("255.255.255.255");
+//        // invalid
+//        testList.add("123.256.2.40");
+//        testList.add("023.35.001.40");
+//        testList.add("123.1.240");
+//        testList.add("0.0.0.0");
+//        testList.add("1");
+//
+//        for (String s : testList) {
+//            System.out.println("Ip: " + s + ", is " + test.checkIpValidityByRegularExpression(s));
+//        }
 
 
         // for question 2
+        List<String> strings = test.allValidIpFormat("010010");
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
     }
 
+
+    public static final Pattern NUMBER_EXPRESSION = Pattern.compile("\\d");
 
     /**
      * 题目2. 找出该字符串合法的ip形式
      */
-//    public List<String> allValidIpFormat(String )
+    public List<String> allValidIpFormat(String inputNumber) {
+
+        List<String> curSlots = new ArrayList<>();
+        List<String> results = new ArrayList<>();
+
+        dfs(inputNumber, 0, curSlots, results);
+
+        return results;
+    }
+
+
+    private void dfs(String totalStr, int curIndex, List<String> curSlots, List<String> allResults) {
+
+        // reach end of input Str, record valid ip address
+        if (totalStr.length() == curIndex && 4 == curSlots.size()) {
+            StringBuilder sb = new StringBuilder();
+            for (String curSlot : curSlots) {
+                sb.append(".").append(curSlot);
+            }
+            allResults.add(sb.substring(1));
+        }
+
+        if (4 == curSlots.size()) {
+            // have 4 slot, but not totally using the input string
+            return;
+        }
+
+        for (int i = curIndex; i < totalStr.length(); i++) {
+            // possible slot selection
+            String chosenSlot = totalStr.substring(curIndex, i + 1);
+            // only when the chosen slot is a valid number
+            if (!isValid(chosenSlot)) {
+                return;
+            }
+            // add success slot
+            curSlots.add(chosenSlot);
+            // doing dfs
+            dfs(totalStr, i + 1, curSlots, allResults);
+            // remove last chosen
+            curSlots.remove(curSlots.size() - 1);
+        }
+    }
+
+    private boolean isValid(String numStr) {
+        if ("".equals(numStr)) {
+            return false;
+        }
+        int parsedNum = Integer.parseInt(numStr);
+        if (parsedNum > 255
+                || (parsedNum != 0 && numStr.startsWith("0"))
+                || (parsedNum == 0 && !"0".equals(numStr))) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
