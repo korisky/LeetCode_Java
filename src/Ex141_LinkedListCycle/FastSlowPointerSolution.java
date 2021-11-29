5e4215683e0d9bcacae359fa9eeb4442a9dc3ba8
lykismekk <lykismekk@outlook.com>
2021-11-29 14:45:02 +0800

    Ex141 finished

---
+++ src/Ex141_LinkedListCycle/FastSlowPointerSolution.java
@@ -0,0 +1,34 @@
+package Ex141_LinkedListCycle;
+
+/**
+ * 经典的快慢指针找循环的解决思路
+ */
+public class FastSlowPointerSolution {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+    public boolean hasCycle(ListNode head) {
+        if (null == head || null == head.next) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
