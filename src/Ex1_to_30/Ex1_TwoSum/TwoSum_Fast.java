003f574771c458cd9c080dd185e189cd4e2bd3ab
lykismekk <lykismekk@outlook.com>
2022-01-14 10:49:22 +0800

    Ex167 finished

--- src/Ex1_to_30/Ex1_TwoSum/TwoSum_Fast.java
+++ src/Ex1_to_30/Ex1_TwoSum/TwoSum_Fast.java
@@ -1,8 +1,8 @@
 package Ex1_to_30.Ex1_TwoSum;
 
-//Given nums = [2, 7, 11, 15], target = 9,
-//Because nums[0] + nums[1] = 2 + 7 = 9,
-//return [0, 1].
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
 
 import java.util.HashMap;
 
