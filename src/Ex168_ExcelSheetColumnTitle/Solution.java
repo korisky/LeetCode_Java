139ae9dd269181c05014e41a218c13f400e33ce2
lykismekk <lykismekk@outlook.com>
2022-01-24 14:55:20 +0800

    Ex1679 finished with sorting idea

---
+++ src/Ex168_ExcelSheetColumnTitle/Solution.java
@@ -0,0 +1,21 @@
+package Ex168_ExcelSheetColumnTitle;
+
+/**
+ * 相对于使用正向的计算, 从右往左取余, 最后reverse的算法速度快很多
+ */
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            int r = n % 26;
+            n = n / 26;
+            if (r != 0) {
+                sb.append((char) (r - 1 + 'A'));
+            } else {
+                sb.append('Z');
+                n--;
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
