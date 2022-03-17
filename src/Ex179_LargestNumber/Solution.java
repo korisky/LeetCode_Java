eff3970ec20aded6d436ddf43a3d8b536e4e68a7
lykismekk <lykismekk@outlook.com>
2022-03-17 23:06:15 +0800

    Ex179 finished with idea from Leetcode

--- src/Ex179_LargestNumber/Solution.java
+++ src/Ex179_LargestNumber/Solution.java
@@ -1,12 +1,11 @@
 package Ex179_LargestNumber;
 
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.*;
 
+/**
+ * 问题重点在于, 实际上可以将比较缩小为两个str的比较, 这样只需要重写一个Comparater, 然后用Arrays.sort即可完成负责目标
+ */
 public class Solution {
-
 
     public String largestNumber(int[] nums) {
 
@@ -14,62 +13,25 @@ public class Solution {
             return "";
         }
 
-        // 存储 0-9, 每个slot是一个list
-        PriorityQueue<Integer>[] array = new PriorityQueue[10];
-        for (int wholeNum : nums) {
-            // 只对第一个字符进行取模
-            String str = String.valueOf(wholeNum);
-            int firstNum = Integer.parseInt(str.substring(0, 1));
-            for (int i = 9; i >= 0; i--) {
-                if (firstNum % i == 0) {
-                    addIntoQueue(array, i, wholeNum);
-                    break;
-                }
-            }
+        String[] numArr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numArr[i] = String.valueOf(nums[i]);
         }
 
-        StringBuilder result = new StringBuilder();
+        // 重点在于, 将两个数字进行比较即可
+        Arrays.sort(numArr, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
 
-        // 把所有数取完
-        // 从9 -> 0, 取数字
-        for (int i = 9; i >= 0; i--) {
-            PriorityQueue<Integer> curSlot = array[i];
-            // a. 没有数字的可以直接跳过
-            if (null == curSlot) {
-                continue;
-            }
-            // b. 只有一个数字也可以简单的添加
-            if (1 == curSlot.size()) {
-                result.append(curSlot.peek());
-                continue;
-            }
-            // c. 多个数字的情况
-            while (!curSlot.isEmpty()) {
-                // 判断最大的数字的第二位, 是否大于该slot原本位
-                Integer peek = curSlot.peek();
-                String str = String.valueOf(peek);
-                int numSecond = Integer.parseInt(str.substring(1, 2));
-                if (numSecond > i) {
-                    result.append(curSlot.poll());
-                } else {
-
-                }
-            }
+        // 针对0开头的, 即使多个全0, 也应该返回0
+        if (numArr[0].charAt(0) == '0') {
+            return "0";
         }
 
-
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (String s : numArr) {
+            sb.append(s);
+        }
+        return sb.toString();
     }
 
-    private void addIntoQueue(PriorityQueue<Integer>[] array, int slotIdx, int wholeNum) {
-//        PriorityQueue<Integer> thisSlot = array[slotIdx - 1];
-//        if (null == thisSlot) {
-//            // queue is empty and not initialized
-//            thisSlot = new PriorityQueue();
-//            thisSlot.addFirst(wholeNum);
-//        } else {
-//            // keep it like priority queue with biggest one in the head
-//            Integer first = thisSlot.getFirst();
-//        }
-    }
+
 }
