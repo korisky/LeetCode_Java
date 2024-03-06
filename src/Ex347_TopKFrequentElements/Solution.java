94f346ca784f22fad8bc2bcfc90f33d5eaa1fe6e
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-03-06 12:19:39 +0800

    chore: add own solution with bucketSort idea for #347

--- src/Ex347_TopKFrequentElements/Solution.java
+++ src/Ex347_TopKFrequentElements/Solution.java
@@ -4,7 +4,63 @@ import java.util.*;
 
 public class Solution {
 
+    /**
+     * 由于统计频率一定不超过输入总数num, 尝试使用固定位置计数,
+     * 类似BucketSort -> 知道最大最小值后, 进行映射即可实现具体idx操作, 方便统计频率
+     */
     public static int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer>[] freqAppearList = new List[nums.length];
+
+        // 初始化Array中的每个元素
+        for (int i = 0; i < freqAppearList.length; i++) {
+            freqAppearList[i] = new ArrayList<>();
+        }
+
+        // 遍历
+        for (int num : nums) {
+            Integer freq = map.getOrDefault(num, 0) + 1;
+            // into list
+            freqAppearList[freq - 1].add(num);
+            // update map
+            map.put(num, freq);
+        }
+
+        // 从频率最高到最低进行遍历
+        int resIdx = 0;
+        int[] res = new int[k];
+        Set<Integer> resSet = new HashSet<>();
+        for (int j = freqAppearList.length - 1; j >= 0; j--) {
+            List<Integer> curList = freqAppearList[j];
+            // 该频次下有数字, 进行添加
+            if (!curList.isEmpty()) {
+                for (Integer theNum : curList) {
+                    // 直到超过数量
+                    if (resIdx >= k) {
+                        break;
+                    }
+                    // 保存过了
+                    if (resSet.contains(theNum)) {
+                        continue;
+                    }
+                    res[resIdx++] = theNum;
+                    resSet.add(theNum);
+                }
+            }
+            // 直到超过数量
+            if (resIdx >= k) {
+                break;
+            }
+        }
+        return res;
+    }
+
+
+    /**
+     * 使用最基础的方式, 结合Map遍历统计频率, 再添加到PriorityQueue中实现大顶堆, 获取前k个最大的元素
+     */
+    public static int[] topKFrequent_WithQueue(int[] nums, int k) {
         // 统计次数
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
@@ -24,8 +80,11 @@ public class Solution {
         return result;
     }
 
+
     public static void main(String[] args) {
-        int[] ints = new int[]{5, 3, 1, 1, 1, 3, 73, 1};
+//        int[] ints = new int[]{5, 3, 1, 1, 1, 3, 73, 1};
+        int[] ints = new int[]{1, 1, 1, 2, 2, 3};
+//        int[] ints1 = topKFrequent_WithQueue(ints, 2);
         int[] ints1 = topKFrequent(ints, 2);
         for (int anInt : ints1) {
             System.out.print(anInt + " ");
