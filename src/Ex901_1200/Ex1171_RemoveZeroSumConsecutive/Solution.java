008d909a82abcb83587521c5f3a26e72018b2441
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-03-12 11:03:47 +0800

    chore: add online simple & clear solution for #1171

--- src/Ex901_1200/Ex1171_RemoveZeroSumConsecutive/Solution.java
+++ src/Ex901_1200/Ex1171_RemoveZeroSumConsecutive/Solution.java
@@ -1,5 +1,7 @@
 package Ex901_1200.Ex1171_RemoveZeroSumConsecutive;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class Solution {
@@ -20,7 +22,7 @@ public class Solution {
         }
     }
 
-    public static ListNode removeZeroSumSubLists(ListNode head) {
+    public static ListNode removeZeroSumSubLists_OnlyForSimpleSum(ListNode head) {
         // conner case
         if (head == null || head.next == null) {
             return head;
@@ -50,6 +52,34 @@ public class Solution {
         return last.next;
     }
 
+    /**
+     * 非常优秀的处理方式
+     */
+    public static ListNode removeZeroSumSubLists(ListNode head) {
+
+        // 通用准备方式
+        int prefix = 0;
+        ListNode dumHead = new ListNode(0);
+        dumHead.next = head;
+
+        // 第一次遍历, 持续累加, 通过计算可以发现, 如果出现累加值相等的情况, 实际上中间就是求和为0 (++--然后重新到同一个值)
+        Map<Integer, ListNode> seen = new HashMap<>();
+        seen.put(0, dumHead);
+        for (ListNode i = dumHead; i != null; i = i.next) {
+            prefix += i.val;
+            seen.put(prefix, i);
+        }
+
+        // 第二次遍历, 继续持续累加, 但此次累加进行取值, 由于上一次遍历时相同的值会被更新, 所以这里直接获取到可以跳过的部分
+        prefix = 0;
+        for (ListNode i = dumHead; i != null; i = i.next) {
+            prefix += i.val;
+            i.next = seen.get(prefix).next;
+        }
+        return dumHead.next;
+    }
+
+
     public static void main(String[] args) {
 
         ListNode last = new ListNode(-5);
@@ -62,9 +92,9 @@ public class Solution {
         ListNode last5 = new ListNode(-4);
         last5.next = last4;
         ListNode last6 = new ListNode(-3);
-        last5.next = last4;
+        last6.next = last5;
         ListNode last7 = new ListNode(1);
-        last5.next = last4;
+        last7.next = last6;
 
 
         ListNode newHead = removeZeroSumSubLists(last5);
