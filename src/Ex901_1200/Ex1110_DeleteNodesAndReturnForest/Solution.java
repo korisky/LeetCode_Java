ffb036a40008b7de6aea9d36b0d526731d14ebcc
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-03-20 18:23:28 +0800

    chore: add solution on #1110

---
+++ src/Ex901_1200/Ex1110_DeleteNodesAndReturnForest/Solution.java
@@ -0,0 +1,60 @@
+package Ex901_1200.Ex1110_DeleteNodesAndReturnForest;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<TreeNode> remaining = new LinkedList<>();
+    Set<Integer> toDelete = new HashSet<>();
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        for (int i : to_delete) {
+            toDelete.add(i);
+        }
+        dfsDel(root, true);
+        return remaining;
+    }
+
+    public TreeNode dfsDel(TreeNode root, boolean isRoot) {
+
+        if (root == null) {
+            return null;
+        }
+
+        boolean needsDel = toDelete.contains(root.val);
+        // 核心难点在这, isRoot是上一个node传递过来的, 如果上一个需要被delete,
+        // 而当前这个节点不需要被delete, 则肯定是remain, 就可以添加
+        if (isRoot && !needsDel) {
+            remaining.add(root);
+        }
+
+        // 而递归调用时, 如果当前被delete了, 那么就会告知下一个节点, 需要变为root
+        root.left = dfsDel(root.left, needsDel);
+        root.right = dfsDel(root.right, needsDel);
+        return needsDel ? null : root;
+    }
+
+
+}
