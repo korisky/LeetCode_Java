2cd85d60b06b32a6bd112946ad28c0ff20d76a6d
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-03-21 11:26:46 +0800

    chore: add tests for #55

--- src/Ex1_300/Ex31_to_60/Ex55_JumpGame/FastestSolution.java
+++ src/Ex1_300/Ex31_to_60/Ex55_JumpGame/FastestSolution.java
@@ -7,12 +7,18 @@ package Ex1_300.Ex31_to_60.Ex55_JumpGame;
  */
 
 public class FastestSolution {
-    public boolean canJump(int[] nums) {
+    public static boolean canJump(int[] nums) {
         int reachFromTail = nums.length - 1;
         for (int i = nums.length - 2; i >= 0; i--) {
             if (i + nums[i] >= reachFromTail)
                 reachFromTail = i;
         }
         return reachFromTail == 0;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(canJump(new int[]{3, 2, 1, 0, 4}));
+        System.out.println(canJump(new int[]{3, 2, 2, 0, 4}));
+//        System.out.println(canJump(new int[]{2, 3, 1, 1, 4}));
     }
 }
