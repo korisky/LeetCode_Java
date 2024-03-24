7440f6d349174f1d9e4fb26fbd301faba1f9017b
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-03-24 13:02:19 +0800

    chore: add comments for #42

--- src/Ex1_300/Ex31_to_60/Ex42_TrappingRainWater/trapWater.java
+++ src/Ex1_300/Ex31_to_60/Ex42_TrappingRainWater/trapWater.java
@@ -16,42 +16,19 @@ Using the method from LeetCode,
 public class trapWater {
     public int trap(int[] height) {
 
-//      // Much more clear version, which is 1ms slower then the below one
-//        int[] fromLeft = new int[height.length];
-//        int[] fromRight = new int[height.length];
-//
-//        int lmax = 0;
-//        for (int l = 0; l < height.length; l++) {
-//            if (height[l] > lmax)
-//                lmax = height[l];
-//            fromLeft[l] = lmax;
-//        }
-//
-//        int rmax = 0;
-//        for (int r = height.length - 1; r >= 0; r--) {
-//            if (height[r] > rmax)
-//                rmax = height[r];
-//            fromRight[r] = rmax;
-//        }
-//
-//        int realVolume = 0;
-//        for (int i = 0; i < height.length; i++) {
-//            int curHeight = Math.min(fromLeft[i], fromRight[i]);
-//            realVolume += curHeight - height[i];
-//        }
-//
-//        return realVolume;
-
         if (height.length == 0)
             return 0;
 
         int[] leftHeight = new int[height.length];
+        int[] rightHeight = new int[height.length];
+
+        // 记住初始化, 对于left初始化左边, right初始化右边, 分别直接就是height挡板在那个位置的高度
         leftHeight[0] = height[0];
+        rightHeight[height.length - 1] = height[height.length - 1];
+
         for (int i = 1; i < height.length; i++)
             leftHeight[i] = Math.max(leftHeight[i - 1], height[i]);
 
-        int[] rightHeight = new int[height.length];
-        rightHeight[height.length - 1] = height[height.length - 1];
         for (int i = height.length - 2; i >= 0; i--)
             rightHeight[i] = Math.max(rightHeight[i + 1], height[i]);
 
