266421f79c260f0ed17d40e141a13a0eed4cdb67
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-03-26 21:42:03 +0800

    chore: add online dynamic programming solution for #403

--- src/Ex601_900/Ex621_TaskScheduler/Solution.java
+++ src/Ex601_900/Ex621_TaskScheduler/Solution.java
@@ -20,11 +20,14 @@ public class Solution {
         // 最多停顿的次数, 一定是某个task出现的频次-1 乘 要等待的n（第一次不需要等待所以-1）
         Arrays.sort(freq);
         int maxFreq = freq[25];
+        // 这里的意思是, 最大频次是两端, 其中间有freq-1个间隙, 所以这里是freq-1, 并且停顿时间是n
         int idleTime = (maxFreq - 1) * n;
 
         // 3. 尽可能的缩减idleTime, 通过其他task进行填补, 从高频次到低频次
         for (int i = 24; i >= 0; i--) {
-            // 这里的min是指, 如果任务频次相同则直接减去中间所有的空位, 如果不是, 那么根据频次进行填补
+            // 这里的min是指填:
+            // 1) 如果freq[i]跟freq[25]相同, 那么这里也并不能减去freq[25]这么多个频次, 只能取每一个间隙, 也就是maxFreq - 1
+            // 2) 如果freq[i]比间隙少, 那就只能减去那么多, 用不满每一个间隙
             idleTime -= Math.min(maxFreq - 1, freq[i]);
         }
         return tasks.length + Math.max(0, idleTime);
