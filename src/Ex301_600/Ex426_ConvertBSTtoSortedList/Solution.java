f8be9d987eaa5196b76e035ce7bd8633fea4fe70
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-03-27 10:13:13 +0800

    chore: add online dfs recursive solution for #426

---
+++ src/Ex301_600/Ex426_ConvertBSTtoSortedList/Solution.java
@@ -0,0 +1,63 @@
+package Ex301_600.Ex426_ConvertBSTtoSortedList;
+
+public class Solution {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+
+    /**
+     * 题目需要把二叉搜索树, 转换为排序的双向链表
+     */
+    public Node treeToDoublyList(Node root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        intervalTraversing(root);
+
+        first.left = last;
+        last.right = first;
+        return first;
+    }
+
+    Node first;
+    Node last;
+
+    public void intervalTraversing(Node root) {
+        if (root != null) {
+            // 先进行左侧遍历
+            intervalTraversing(root.left);
+            // 中序部分进行链接
+            if (last != null) {
+                // 如果last不为空, 将其指向该节点
+                last.right = root;
+                root.left = last;
+            } else {
+                // 如果last为空, 可以认为是还没有链接节点, 将first置为自己
+                first = root;
+            }
+            // 将last指向自己
+            last = root;
+            // 进行右侧遍历
+            intervalTraversing(root.right);
+        }
+
+    }
+
+}
