4465041db2b1b5a686db027ae63cdb8d6daf6e05
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-04-04 14:00:03 +0800

    chore: add online simple Stack based solution for #1614

---
+++ src/Ex1614_MaximumNestingDepthParentheses/Solution.java
@@ -0,0 +1,27 @@
+package Ex1614_MaximumNestingDepthParentheses;
+
+import java.util.Stack;
+
+public class Solution {
+
+    /**
+     * 题目复杂, 但是实际上只需要使用stack进行处理
+     */
+    public static int maxDepth(String s) {
+        int ans = 0;
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                st.push(c);
+            } else if (c == ')') {
+                st.pop();
+            }
+            ans = Math.max(ans, st.size());
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxDepth("(1+(2*3)+((8)/4))+1"));
+    }
+}
