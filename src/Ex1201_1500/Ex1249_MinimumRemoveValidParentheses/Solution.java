037ff9a501fe56bc5c22562df213cc5134d7fd69
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-04-06 22:18:05 +0800

    chore: cmt for #1249

--- src/Ex1201_1500/Ex1249_MinimumRemoveValidParentheses/Solution.java
+++ src/Ex1201_1500/Ex1249_MinimumRemoveValidParentheses/Solution.java
@@ -6,13 +6,19 @@ import java.util.Stack;
 public class Solution {
 
     public static void main(String[] args) {
-//        String text = "lee(t(c)o)de)";
-        String text = "a)b(c)d";
+        String text = "lee(t(c)o)de)";
+//        String text = "a)b(c)d";
 //        String text = "))((";
         System.out.println(minRemoveToMakeValid(text));
     }
 
 
+    /**
+     * 题目需要构造一个最小修改得来的validString, 使得左右括号位置合理
+     * 核心是使用Stack进行存储, 每次遇到(进行index的存储, 遇到)则尝试pop出来,
+     * 如果pop出来的是)那么这两个都不用存到stack, 不合理的index都会存到stack,
+     * 最后重新遍历string, 遇到这些还在stack中的肯定就不是合理的
+     */
     public static String minRemoveToMakeValid(String s) {
 
         Stack<Integer> charIdxStack = new Stack<>();
