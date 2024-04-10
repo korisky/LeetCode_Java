99a61d299acd0f8489b7b109124166f0b3c1db97
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-04-10 09:00:55 +0800

    chore: add online simple solution for #2073

---
+++ src/Ex2073_TimeNeededBuyTickets/Solution.java
@@ -0,0 +1,27 @@
+package Ex2073_TimeNeededBuyTickets;
+
+public class Solution {
+
+    /**
+     * 类似Ex1700, 该算法的核心是直接对数组进行遍历而不是进行操作的模拟 (但是Array的order是需要保留的)
+     * - 对于排在target之前的人, 需要买的票就是 min(自己要买的, target要买的), 因为如果自己买的少那么后面就跳出队伍了
+     * - 对于排在target之后的人, 需要买的票就是 min(自己要买的, target要买的 - 1), 这里的-1是因为, target买好自己票之后不再管后面的人了, 所以他们额外买(即使同一轮次), target也不等了
+     */
+    public static int timeRequiredToBuy(int[] tickets, int k) {
+        int times = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                times += Math.min(tickets[i], tickets[k]);
+            } else {
+                times += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+        return times;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(timeRequiredToBuy(new int[]{2, 3, 2}, 2));
+        System.out.println(timeRequiredToBuy(new int[]{5, 1, 1, 1}, 0));
+        System.out.println(timeRequiredToBuy(new int[]{5, 1, 1, 2, 3}, 0));
+    }
+}
