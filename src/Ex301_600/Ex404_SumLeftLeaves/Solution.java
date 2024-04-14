375935fc54d0f7799a302240375eace6ba4715c3
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-04-14 14:59:56 +0800

    chore: add simple solution for #404

---
+++ src/Ex301_600/Ex404_SumLeftLeaves/Solution.java
@@ -0,0 +1,38 @@
+package Ex301_600.Ex404_SumLeftLeaves;
+
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int ans = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                ans += root.left.val;
+            } else {
+                ans += sumOfLeftLeaves(root.left);
+            }
+        }
+        ans += sumOfLeftLeaves(root.right);
+        return ans;
+    }
+}
