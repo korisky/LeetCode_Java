11fdc055aea660647d1e3d11fb212cd75b61187b
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-05-01 11:23:35 +0800

    chore: add own simple stack solution for #2000

---
+++ src/Ex2000_ReversePrefixWord/Solution.java
@@ -0,0 +1,46 @@
+package Ex2000_ReversePrefixWord;
+
+import java.util.Stack;
+
+public class Solution {
+
+    /**
+     * 使用Stack进行倒置
+     */
+    public static String reversePrefix(String word, char ch) {
+
+
+        int idx = 0;
+        char[] charArr = word.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        boolean found = false;
+        for (char c : charArr) {
+            idx++;
+            stack.push(c);
+            if (c == ch) {
+                found = true;
+                break;
+            }
+        }
+
+        // 不包含字符的特殊情况
+        if (!found) {
+            return word;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        for (; idx < charArr.length; idx++) {
+            sb.append(charArr[idx]);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reversePrefix("abcdefd", 'd'));
+    }
+}
