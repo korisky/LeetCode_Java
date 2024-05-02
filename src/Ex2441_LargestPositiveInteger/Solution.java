464c759751fa4607f6beb01568387d3d235a91ca
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-05-02 17:28:14 +0800

    chore: add own simple solution for #2441

---
+++ src/Ex2441_LargestPositiveInteger/Solution.java
@@ -0,0 +1,23 @@
+package Ex2441_LargestPositiveInteger;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public static int findMaxK(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                max = Math.max(max, Math.max(num, -num));
+            }
+            set.add(-num);
+        }
+        return max > 0 ? max : -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMaxK(new int[]{-1, 2, -3, 3}));
+    }
+}
