7016dfb24bac35dc9a1e125da95e1b87ffc335e7
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-05-12 10:18:04 +0800

    chore: add brut force solution for #2373

---
+++ src/Ex2373_LargestLocalValuesInMatrix/Solution.java
@@ -0,0 +1,31 @@
+package Ex2373_LargestLocalValuesInMatrix;
+
+public class Solution {
+
+    /**
+     * 没有DP的方式, 直接brut force实现
+     */
+    public int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] maxLocal = new int[n - 2][n - 2];
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                maxLocal[i][j] = findMax(grid, i, j);
+            }
+        }
+        return maxLocal;
+    }
+
+    /**
+     * 寻找3x3矩阵内的最大值
+     */
+    private int findMax(int[][] grid, int x, int y) {
+        int maxEle = 0;
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                maxEle = Math.max(maxEle, grid[i][j]);
+            }
+        }
+        return maxEle;
+    }
+}
