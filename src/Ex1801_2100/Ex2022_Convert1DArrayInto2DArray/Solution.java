ed5ce5a9bf51bd55fbb608e53047d64026468f42
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-09-01 23:12:40 +0800

    chore: add faster row copy solution on #2022

--- src/Ex1801_2100/Ex2022_Convert1DArrayInto2DArray/Solution.java
+++ src/Ex1801_2100/Ex2022_Convert1DArrayInto2DArray/Solution.java
@@ -1,5 +1,7 @@
 package Ex1801_2100.Ex2022_Convert1DArrayInto2DArray;
 
+import java.util.Arrays;
+
 public class Solution {
 
     public static int[][] construct2DArray(int[] original, int m, int n) {
@@ -19,7 +21,21 @@ public class Solution {
         return res;
     }
 
+    public static int[][] construct2DArray_Faster(int[] original, int m, int n) {
+        if (original.length != m * n) {
+            return new int[0][];
+        }
+        int[][] res = new int[m][n];
+        for (int r = 0; r < m; r++) {
+            res[r] = Arrays.copyOfRange(original, r * n, r * n + n);
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(construct2DArray(new int[]{1, 2, 3, 4}, 2, 2));
+        int[][] slow = construct2DArray(new int[]{1, 2, 3, 4}, 2, 2);
+        int[][] fast = construct2DArray_Faster(new int[]{1, 2, 3, 4}, 2, 2);
+        System.out.println();
     }
 }
