811bc6aa7a5bae44504f9d480f6d769d7c55bd77
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-10-17 22:30:10 +0800

    chore: add Leetcode's idea with greedy (unit test) for #670

--- src/Ex601_900/Ex670_MaximumSwap/Solution.java
+++ src/Ex601_900/Ex670_MaximumSwap/Solution.java
@@ -5,16 +5,18 @@ public class Solution {
     /**
      * 转换2个数字的位置, 来达到最大值
      */
-    public int maximumSwap(int num) {
+    public static int maximumSwap(int num) {
         String numStr = Integer.toString(num);
         int n = numStr.length();
         int[] lastSeen = new int[10];
 
+        // 先遍历一遍, 确保0-9遇到过的数字
         for (int i = 0; i < n; ++i) {
             lastSeen[numStr.charAt(i) - '0'] = i;
         }
 
         for (int i = 0; i < n; i++) {
+            // 从高到底开始, 针对遇到过的, 然后当前高位低过它的, 进行swap
             for (int d = 9; d > numStr.charAt(i) - '0'; d--) {
                 if (lastSeen[d] > i) {
                     char[] arr = numStr.toCharArray();
@@ -27,5 +29,9 @@ public class Solution {
             }
         }
         return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maximumSwap(1058));
     }
 }
