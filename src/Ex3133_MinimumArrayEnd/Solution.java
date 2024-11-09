35ef9e577ddf17f9cba47e6a1ebc98f8b49b6bb8
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-11-09 12:14:07 +0800

    chore: add Leetcode's idea about using bit manipulation on #3133

---
+++ src/Ex3133_MinimumArrayEnd/Solution.java
@@ -0,0 +1,17 @@
+package Ex3133_MinimumArrayEnd;
+
+public class Solution {
+
+    public long minEnd(int n, int x) {
+        long result = x;
+        long mask;
+        n--;
+        for (mask = 1; n > 0; mask <<= 1) {
+            if ((mask & x) == 0) {
+                result |= (n & 1) * mask;
+                n >>= 1;
+            }
+        }
+        return result;
+    }
+}
