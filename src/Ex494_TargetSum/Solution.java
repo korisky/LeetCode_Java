7dd2dca52cbe79cc5fb75dfa331c21acc5df6d63
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-12-26 17:44:03 +0800

    chore: add solution from Leetcode's idea with Brute-force method on Ex494

---
+++ src/Ex494_TargetSum/Solution.java
@@ -0,0 +1,31 @@
+package Ex494_TargetSum;
+
+/**
+ * Brute-force method -> go through all possibilities
+ */
+public class Solution {
+
+
+    int totalWays = 0;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        calculateWays(nums, 0, 0, target);
+        return totalWays;
+    }
+
+    private void calculateWays(int[] nums, int curIdx, int curSum, int target) {
+        // max idx
+        if (curIdx == nums.length) {
+            // check if current sum just match the target
+            if (curSum == target) {
+                totalWays++;
+            }
+            // idx exceed
+            return;
+        }
+        // include current number with positive sign
+        calculateWays(nums, curIdx + 1, curSum + nums[curIdx], target);
+        // include current number with negative sign
+        calculateWays(nums, curIdx + 1, curSum - nums[curIdx], target);
+    }
+}
