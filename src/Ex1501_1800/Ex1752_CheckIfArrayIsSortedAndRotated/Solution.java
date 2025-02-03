a0e92ab4a670c4ce9965f63dcb466b12a9fc69ba
Roylic <35546172+korisky@users.noreply.github.com>
2025-02-03 15:40:50 +0800

    chore: add solution with LeetCode's idea on Ex1752

---
+++ src/Ex1501_1800/Ex1752_CheckIfArrayIsSortedAndRotated/Solution.java
@@ -0,0 +1,21 @@
+package Ex1501_1800.Ex1752_CheckIfArrayIsSortedAndRotated;
+
+public class Solution {
+
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        if (n <= 1) {
+            return true;
+        }
+        int inversionCount = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                inversionCount++;
+            }
+        }
+        if (nums[0] < nums[n - 1]) {
+            inversionCount++;
+        }
+        return inversionCount <= 1;
+    }
+}
